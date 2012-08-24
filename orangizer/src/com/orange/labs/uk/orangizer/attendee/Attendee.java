5a17005ce59056961955cbdff6f24c227d1c8345
Jean-Francois Moy <jeanfrancois.moy@gmail.com>
2012-08-24 17:19:31 +0100

    Started implementing attendees support for events.

---
+++ orangizer/src/com/orange/labs/uk/orangizer/attendee/Attendee.java
@@ -0,0 +1,133 @@
+package com.orange.labs.uk.orangizer.attendee;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.orange.labs.uk.orangizer.event.RsvpStatus;
+import com.orange.labs.uk.orangizer.utils.Logger;
+
+/**
+ * Attendee for an Event.
+ */
+public class Attendee {
+	
+	private static final Logger sLogger = Logger.getLogger(Attendee.class);
+
+	private final String mName;
+
+	private final RsvpStatus mStatus;
+
+	private final String mFacebookId;
+
+	private final String mEmail;
+
+	private final String mPhoneNumber;
+
+	private final String mTwitterUsername;
+
+	private Attendee(String name, String email, String phoneNumber, RsvpStatus status,
+			String facebookId, String twitterUsername) {
+		mName = name;
+		mStatus = status;
+		mFacebookId = facebookId;
+		mEmail = email;
+		mPhoneNumber = phoneNumber;
+		mTwitterUsername = twitterUsername;
+	}
+
+	public boolean hasName() {
+		return (mName != null);
+	}
+
+	public boolean hasStatus() {
+		return (mStatus != null);
+	}
+	
+	public String getName() {
+		return mName;
+	}
+
+	public RsvpStatus getStatus() {
+		return mStatus;
+	}
+
+	public String getFacebookId() {
+		return mFacebookId;
+	}
+
+	public String getEmail() {
+		return mEmail;
+	}
+
+	public String getPhoneNumber() {
+		return mPhoneNumber;
+	}
+
+	public String getTwitterUsername() {
+		return mTwitterUsername;
+	}
+
+	public static class Builder {
+
+		private String mName;
+
+		private RsvpStatus mStatus;
+
+		private String mFacebookId;
+
+		private String mPhoneNumber;
+
+		private String mEmail;
+
+		private String mTwitterUsername;
+
+		public Builder setName(String name) {
+			mName = name;
+			return this;
+		}
+
+		public Builder setStatus(RsvpStatus status) {
+			mStatus = status;
+			return this;
+		}
+
+		public Builder setFacebookId(String facebookId) {
+			mFacebookId = facebookId;
+			return this;
+		}
+
+		public Builder setEmail(String email) {
+			mEmail = email;
+			return this;
+		}
+
+		public Builder setPhoneNumber(String phoneNumber) {
+			mPhoneNumber = phoneNumber;
+			return this;
+		}
+
+		public Builder setTwitterUsername(String twitterUsername) {
+			mTwitterUsername = twitterUsername;
+			return this;
+		}
+
+		public Attendee build() {
+			return new Attendee(mName, mEmail, mPhoneNumber, mStatus, mFacebookId,
+					mTwitterUsername);
+		}
+
+		public Builder fromJsonObject(JSONObject object) {
+			try {
+				mFacebookId = object.has("id") ? object.getString("id") : null;
+				mName = object.has("name") ? object.getString("name") : null;
+				// TODO: rsvp status
+			} catch (JSONException e) {
+				sLogger.w(String.format("This should never happen: %s", e));
+			}
+
+			return this;
+		}
+
+	}
+
+}
--- orangizer/src/com/orange/labs/uk/orangizer/event/FacebookEventFetcher.java
+++ orangizer/src/com/orange/labs/uk/orangizer/event/FacebookEventFetcher.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,6 +15,7 @@ import com.facebook.android.AsyncFacebookRunner;
 import com.facebook.android.AsyncFacebookRunner.RequestListener;
 import com.facebook.android.Facebook;
 import com.facebook.android.FacebookError;
+import com.orange.labs.uk.orangizer.attendee.Attendee;
 import com.orange.labs.uk.orangizer.callback.Callback;
 import com.orange.labs.uk.orangizer.fetch.EventFetcher;
 import com.orange.labs.uk.orangizer.utils.Logger;
@@ -24,10 +26,10 @@ import com.orange.labs.uk.orangizer.utils.Logger;
 public class FacebookEventFetcher implements EventFetcher {
 
 	private static final Logger sLogger = Logger.getLogger(FacebookEventFetcher.class);
-	
+
 	private final Facebook mFacebook;
 	private final EventsDatabase mEventsDb;
-	
+
 	public FacebookEventFetcher(Facebook facebook, EventsDatabase eventsDatabase) {
 		mFacebook = facebook;
 		mEventsDb = eventsDatabase;
@@ -42,22 +44,22 @@ public class FacebookEventFetcher implements EventFetcher {
 	private class EventsRequestListener implements RequestListener {
 
 		private final Callback<List<Event>> mCallback;
-		
+
 		private EventsRequestListener(final Callback<List<Event>> callback) {
 			mCallback = callback;
 		}
-		
+
 		@Override
 		public void onComplete(String response, Object state) {
 			List<Event> events = new ArrayList<Event>();
-			
+
 			sLogger.d(response);
 			try {
 				JSONObject json = new JSONObject(response);
 				JSONArray jsonArray = json.getJSONArray("data");
 
 				int eventsNumber = jsonArray.length();
-				
+
 				for (int i = 0; i < eventsNumber; i++) {
 					Event event = new Event.Builder().fromJsonObject(jsonArray.getJSONObject(i))
 							.build();
@@ -67,17 +69,22 @@ public class FacebookEventFetcher implements EventFetcher {
 			} catch (JSONException e) {
 				sLogger.w(e.getMessage());
 			}
-			
+
 			// Insert all events in the database.
 			if (events != null) {
-				//TODO: remove the delete all and do a diff instead.
-				mEventsDb.deleteAll();
+				AtomicInteger remaining = new AtomicInteger(events.size());
 				for (Event event : events) {
+					// Insert event locally
 					mEventsDb.insert(event);
+
+					// Prepare path
+					String path = String.format("%s/attending", event.getId());
+
+					// Fetch attendees
+					AsyncFacebookRunner runner = new AsyncFacebookRunner(mFacebook);
+					runner.request(path, new AttendeesRequestListener(event, mCallback, remaining));
 				}
 			}
-			
-			mCallback.onSuccess(events);
 		}
 
 		@Override
@@ -99,10 +106,82 @@ public class FacebookEventFetcher implements EventFetcher {
 		public void onFacebookError(FacebookError e, Object state) {
 			handleFailure(e);
 		}
-		
+
 		private void handleFailure(Exception e) {
 			sLogger.w(e.toString());
 			mCallback.onFailure(e);
+		}
+
+	}
+
+	/**
+	 * {@link RequestListener} for Attendees request for a particular events. It parses the
+	 * attendees and inserts them along the event.
+	 */
+	private class AttendeesRequestListener implements RequestListener {
+
+		/** Callback to execute when everything has been done or an error has occured */
+		private final Callback<List<Event>> mCallback;
+
+		/** Indicates how many requests are left */
+		private final AtomicInteger mRemaining;
+
+		/** Event we request the attendees of */
+		private final Event mEvent;
+
+		public AttendeesRequestListener(Event event, Callback<List<Event>> callback,
+				AtomicInteger remaining) {
+			mEvent = event;
+			mCallback = callback;
+			mRemaining = remaining;
+		}
+
+		@Override
+		public void onComplete(String response, Object state) {
+			try {
+				JSONObject json = new JSONObject(response);
+				JSONArray jsonArray = json.getJSONArray("data");
+
+				int attendeesNumber = jsonArray.length();
+				List<Attendee> attendees = new ArrayList<Attendee>(attendeesNumber);
+				for (int i = 0; i < attendeesNumber; i++) {
+					Attendee attendee = new Attendee.Builder().fromJsonObject(
+							jsonArray.getJSONObject(i)).build();
+					attendees.add(attendee);
+				}
+				
+				sLogger.i(attendees.toString());
+			} catch (JSONException e) {
+				sLogger.w(e.getMessage());
+			}
+			
+			if (mRemaining.decrementAndGet() == 0) {
+				mCallback.onSuccess(null);
+			}
+		}
+
+		@Override
+		public void onIOException(IOException e, Object state) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onFileNotFoundException(FileNotFoundException e, Object state) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onMalformedURLException(MalformedURLException e, Object state) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onFacebookError(FacebookError e, Object state) {
+			// TODO Auto-generated method stub
+
 		}
 
 	}
